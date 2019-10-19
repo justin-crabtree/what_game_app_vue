80ec189f922c76b6c8666d2e658f544e9f09ac71
crabtree <justin.kyle.crabtree@gmail.com>
2019-10-18 22:20:35 -0400

    initial commit

---
+++ src/views/games/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="games-index">
+    <h1>{{ message }}</h1>
+    <div v-for="game in games">
+      <h2>{{ game.title }}</h2>
+      <img :src="game.image_url" alt="" />
+      <p>{{ game.summary }}</p>
+      <p>Video Url: {{ game.video_url }}</p>
+    </div>
+  </div>
+</template>
+
+<style>
+img {
+  width: 500px;
+}
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      message: "Games Index",
+      games: []
+    };
+  },
+  created: function() {
+    axios.get("/api/games").then(response => {
+      this.games = response.data;
+    });
+  },
+  methods: {}
+};
+</script>
